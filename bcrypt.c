9b36ff5cde609c049367479d4163a8efa5c4c737
steinuil <steenuil.owl@gmail.com>
2018-01-23 21:44:37 +0100

    fixed hash generation

--- bcrypt.c
+++ bcrypt.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <string.h>
 #include <urweb.h>
 
 #include "bcrypt.h"
@@ -9,27 +9,55 @@
 #define BCRYPT_GENSALT_SIZE (7 + 22 + 1)
 
 
-uw_Basis_string uw_Bcrypt_hash(uw_context ctx, uw_Basis_string password) {
-  char data[16];
+uw_Basis_string uw_Bcrypt_genSalt(uw_context ctx, uw_Basis_int cost) {
+  size_t rand_size = 16;
+  char rand_bytes[rand_size];
   { FILE *urand = fopen("/dev/urandom", "r");
-    fread(&data, sizeof(char), 16, urand);
-    fclose(urand);
+    if (!urand) {
+      uw_error(ctx, FATAL, "Failed to open /dev/urandom");
+    }
+
+    size_t read = fread(&rand_bytes, sizeof(char), rand_size, urand);
+    if (!read || (read / sizeof(char)) < rand_size) {
+      fclose(urand);
+      uw_error(ctx, BOUNDED_RETRY, "Failed to read from /dev/urandom");
+    }
+
+    int ok = fclose(urand);
+    if (ok != 0) {
+      uw_error(ctx, BOUNDED_RETRY, "Failed to close /dev/urandom");
+    }
   }
 
-  int hash_size = BCRYPT_HASH_SIZE;
+  char *out = uw_malloc(ctx, BCRYPT_GENSALT_SIZE);
+  { char *ok = crypt_gensalt_rn("$2b$", cost, rand_bytes, rand_size, out, BCRYPT_GENSALT_SIZE);
+    if (ok == NULL) {
+      uw_error(ctx, BOUNDED_RETRY, "Failed to generate salt");
+    }
+  }
 
-  char *salt = crypt_gensalt_ra("$2b$", 10, data, BCRYPT_GENSALT_SIZE);
-  char *hash = crypt_ra(password, salt, NULL, &hash_size);
+  return out;
+}
 
-  char *res = uw_malloc(ctx, hash_size);
-  memcpy(res, hash, hash_size);
 
-  if (salt) free(salt);
-  if (hash) free(hash);
+uw_Basis_string uw_Bcrypt_hash(uw_context ctx, uw_Basis_string password) {
+  char *salt = uw_Bcrypt_genSalt(ctx, 10);
 
-  return res;
+  char *hash = uw_malloc(ctx, BCRYPT_HASH_SIZE);
+  memset(hash, 0, BCRYPT_HASH_SIZE);
+
+  { char *ok = crypt_rn(password, salt, hash, BCRYPT_HASH_SIZE);
+    if (ok == NULL) {
+      uw_error(ctx, BOUNDED_RETRY, "Failed to generate hash");
+    }
+  }
+
+  uw_free(salt);
+
+  return hash;
 }
 
 uw_Basis_bool uw_Bcrypt_check(uw_context ctx, uw_Basis_string password, uw_Basis_string hash) {
+
   return uw_Basis_True;
 }
